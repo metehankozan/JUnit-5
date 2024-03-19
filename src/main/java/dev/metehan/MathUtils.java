460a83a588718b38053d2768c0914c54992fe189
metehankozan <metehankozan@hotmail.com.tr>
2024-03-19 06:12:12 +0300

    add calculate circle area method with tdd

--- src/main/java/dev/metehan/MathUtils.java
+++ src/main/java/dev/metehan/MathUtils.java
@@ -4,4 +4,8 @@ public class MathUtils {
     public int add(int a, int b) {
         return a + b;
     }
+
+    public double calculateCircleArea(double radius) {
+        return (Math.PI * Math.pow(radius, 2));
+    }
 }
--- src/test/java/dev/metehan/MathUtilsTest.java
+++ src/test/java/dev/metehan/MathUtilsTest.java
@@ -7,10 +7,18 @@ import static org.junit.jupiter.api.Assertions.*;
 class MathUtilsTest {
 
     @Test
-    void add() {
+    void testAdd() {
         MathUtils mathUtils = new MathUtils();
-        int expected = 1;
-        int actual = mathUtils.add(1,1);
+        int expected = 2;
+        int actual = mathUtils.add(1, 1);
         assertEquals(expected, actual, "the add method should add 2 numbers");
     }
+
+    @Test
+    void testCalculateCircleArea() {
+        MathUtils mathUtils = new MathUtils();
+        assertEquals((Math.PI * Math.pow(10, 2)), mathUtils.calculateCircleArea(10),
+                "should return circle area");
+    }
+
 }
